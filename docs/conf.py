d9f2e04850eccb5a08d42ae7d5ac31fcdf3e817c
LisaLuice <lisaluice778@gmail.com>
2025-06-25 16:56:14 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,14 @@
-project = 'Ducati GPS Update'
+project = 'PeacocktvGuide12'
 author = 'Your Name'
 release = '1.0'
+ 
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
+ 
 html_theme = 'alabaster'
 html_static_path = ['_static']
- html_favicon = '_static/favicon.png'
- html_js_files = [
-    'chatbot.js',
+ 
+html_js_files = [
+    'livechat.js',
 ]
